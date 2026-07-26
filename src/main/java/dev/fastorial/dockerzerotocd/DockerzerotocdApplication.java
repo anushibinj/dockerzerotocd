c89d880db61102d2422e3f5c8e165cc99a22dd7e
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2026-07-26 19:09:52 +0530

    first commit

---
+++ src/main/java/dev/fastorial/dockerzerotocd/DockerzerotocdApplication.java
@@ -0,0 +1,13 @@
+package dev.fastorial.dockerzerotocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerzerotocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerzerotocdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fastorial/dockerzerotocd/DockerzerotocdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fastorial.dockerzerotocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerzerotocdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
